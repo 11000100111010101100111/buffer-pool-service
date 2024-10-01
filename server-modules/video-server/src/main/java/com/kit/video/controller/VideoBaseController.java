1998557e50d6b39c7c3ac28a8acbb87a12ff96bb
Xiao <2749984520@qq.com>
2024-10-01 10:52:25 +0800

    feat(bufferpool):Adjust project structure

---
+++ server-modules/video-server/src/main/java/com/kit/video/controller/VideoBaseController.java
@@ -0,0 +1,40 @@
+package com.kit.video.controller;
+
+import com.kit.common.config.SystemConfig;
+import com.kit.common.core.domain.AjaxResult;
+import com.kit.common.utils.file.FileUploadUtils;
+import com.kit.framework.util.CommonUploadUtil;
+import io.kit.file.FileCompressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/video")
+public class VideoBaseController {
+    @Autowired
+    private CommonUploadUtil commonUploadUtil;
+
+    @Autowired
+    private FileCompressService compressService;
+
+    @PostMapping("/upload")
+    public AjaxResult upload(MultipartFile file) {
+        String filePath = SystemConfig.getVideoPath();
+        String fileName = FileUploadUtils.extractFilename(file);
+        AjaxResult upload = commonUploadUtil.upload(file, filePath);
+
+        //创建异步任务，压缩视频
+        try {
+            String absolutePath = FileUploadUtils.getAbsoluteFile(filePath, fileName)
+                    .getAbsolutePath();
+
+            //@todo
+        } catch (Exception e) {
+            //do nothing
+        }
+        return upload;
+    }
+}
